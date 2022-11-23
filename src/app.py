c99822f0f0215e9b871f09d5c669bc2eded9b095
Ilias Dinia <iliasd752@gmail.com>
2022-11-23 20:11:41 +0000

    Implementing get, post and delete routes

---
+++ src/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+
+todos = [
+{
+    "label": "My first task",
+    "done": False
+},
+{
+    "label": "My second task",
+    "done": False
+}
+]
+
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    json_text = jsonify(todos)
+    return json_text
+
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    decoded_object = json.loads(request_body)
+    todos.append(decoded_object)
+    print("Incoming request with the following body", request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    return 'something'
+
+
+
+
+# pipenv run python src/app.py
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
